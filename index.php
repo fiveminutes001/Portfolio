bea9e2ca0bf9a981a68a51327696f6da27325dee
five minutes <fiveminutes001@gmail.com>
2021-08-12 18:21:00 +0300

    updated file

--- index.php
+++ index.php
@@ -629,7 +629,14 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
 						</div>
 					</div>
 				</a>
-		
+
+				<a href="http://crockford.com/javascript/">
+					<div class="w3-tag w3-round w3-blue" style="padding:3px;margin-top:6px;">
+						<div class="w3-tag w3-round w3-blue w3-border w3-border-white" style="margin-top:0px;">
+							JavaScript: The Good Parts by Douglas Crockford
+						</div>
+					</div>
+				</a>
 			</div>
 			
 			<div class="column" style="margin-bottom:6px;">
