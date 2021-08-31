1a21687f9f4d4492dc2642c7ca95d70e49800075
five minutes <fiveminutes001@gmail.com>
2021-08-31 12:00:43 +0300

    update

--- js/TopNav.js
+++ js/TopNav.js
@@ -1,7 +1,14 @@
+window.onscroll = function () {
+	myFunction();
+};
+
+var navbar = document.getElementById('navbar');
+var sticky = navbar.offsetTop;
+
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add('sticky');
+	} else {
+		navbar.classList.remove('sticky');
+	}
+}
