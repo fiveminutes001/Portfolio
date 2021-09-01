50799ddc11d0ab49345f1710a9290f4a802b5ded
five minutes <fiveminutes001@gmail.com>
2021-09-01 14:28:45 +0300

    update

---
+++ js/BasicJs.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function (e) {
+	let elements = document.querySelectorAll('div[id^=]');
+	console.log('elements:', elements);
+	console.log('e:', e);
+});
