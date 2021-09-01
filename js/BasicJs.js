7d17e6bb9d3d6ec9bb71124df718983089379787
five minutes <fiveminutes001@gmail.com>
2021-09-01 14:47:31 +0300

    update

--- js/BasicJs.js
+++ js/BasicJs.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', function (e) {
 	let elements = document.querySelectorAll('div[id]');
+	let elementsArray = Array.from(elements);
+
+	const fragment = document.createDocumentFragment();
+	for (let i = 4; i < elementsArray.length; i++) {
+		const newElement = document.createElement('div');
+		newElement.className = 'insideHeaderDiv';
+		fragment.appendChild(newElement);
+		elementsArray.appendChild(fragment);
+	}
+
 	console.log('elements:', elements);
 	console.log('e:', e);
 });
