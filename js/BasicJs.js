fbe69206c006aa0841a74c4c82d6a9f527c0e9c3
five minutes <fiveminutes001@gmail.com>
2021-09-01 16:34:17 +0300

    update

--- js/BasicJs.js
+++ js/BasicJs.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function (e) {
 	debugger;
 	let elements = document.querySelectorAll('div[id]');
-	let elementsArray = Array.from(elements);
+	// let elementsArray = Array.from(elements);
+	// let elemenay.from(elements);
 
-	// const fragment = document.createDocumentFragment();
-	// for (let i = 4; i < elementsArray.length; i++) {
-	// 	//Create new div
-	// 	const newElement = document.createElement('div');
-	// 	newElement.className = 'insideHeaderDiv';
-	// 	// Add class to parent div
-	// 	elementsArray[i].classList.add('headerDiv');
-	// 	// Get the first child node of an element
-	// 	let item = elementsArray[i].childNodes[1];
+	const fragment = document.createDocumentFragment();
 
-	// 	//Duplicate it
-	// 	fragment.appendChild(item);
-	// 	elementsArray[i].appendChild(fragment);
-	// 	// Replace it with the new element
-	// 	item.replaceChild(newElement, item.childNodes[1]);
-	//}
+	for (let i = 4; i < elementsArray.length; i++) {
+		//Create new div
+		const newElement = document.createElement('div');
+		newElement.className = 'insideHeaderDiv';
+		// Add class to parent div
+		elements[i].classList.add('headerDiv');
+		//Get second child
+		let item = elements[i].childNodes[1];
+		// Insert new div before second child
+		elements[i].insertBefore(newElement, item);
+		// //Duplicate it
+		// fragment.appendChild(item);
+		// elementsArray[i].appendChild(fragment);
+		// // Replace it with the new element
+		// item.replaceChild(newElement, item.childNodes[1]);
+	}
 });
